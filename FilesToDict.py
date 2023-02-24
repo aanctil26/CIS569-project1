bcf60cce101b27608bc65fd9fdeca5383219b394
benpfeffer <55898447+benpfeffer@users.noreply.github.com>
2023-02-24 12:56:44 -0500

    Better comments

--- FilesToDict.py
+++ FilesToDict.py
@@ -1,8 +1,12 @@
+# Import required libraries
 import os
 import glob
 import json
 
+# Initialize dictionary
 fileDict = {"CIA":{}, "DIA":{}, "FBI":{}, "NSA":{}, "USCBP":{}}
+
+# Iterate through files, if not correct file (.txt extension) then skip, otherwise add to correct location in dictionary
 for infile in glob.glob(os.path.join("./dataset", '*')):
     if(infile.endswith(".txt")):
         continue
@@ -11,6 +15,7 @@ for infile in glob.glob(os.path.join("./dataset", '*')):
     mainKey = subKey.split("_")[0]
     fileDict[mainKey][subKey] = review_file
 
+# Export dictionary to json file
 with open('CIS569data.json', 'w') as fp:
     json.dump(fileDict, fp)
 
